773b19f761f13e1ff4dba563e1679dcac32b6078
Yaya HC <hamedcuenca5@gmail.com>
2026-08-15 04:08:05 +0000

    refactor: remove unused resolveQuiz method and simplify Wake widget structure

--- wake/lib/services/ios_alarm_bridge.dart
+++ wake/lib/services/ios_alarm_bridge.dart
@@ -3,6 +3,8 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
+import 'pending_quiz.dart';
+
 
 class IosAlarmBridge {
   IosAlarmBridge._();
